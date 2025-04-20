f99249f4987193c8c42f4d27e20683e8f616028d
programersanz <elprogramer69@gmail.com>
2025-04-19 21:33:28 -0500

    CREANDO ANDO

--- LEYLUDICA/LEYLUDICA/settings.py
+++ LEYLUDICA/LEYLUDICA/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ewq4k4-j&yi%(z(4l=5t=j&k)4*6jqdonm&#)1vv)u995s7q*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -91,6 +92,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+import os
+
+DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+
 
 
 # Password validation
@@ -128,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'LEYLUDICA', 'horas', 'static')]
 
 # Default primary key field type
